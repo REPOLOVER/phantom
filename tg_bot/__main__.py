de4cca8e13aa2e2ef70cdb00bbf2740b713ca7e0
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-04-25 20:21:34 +0530

    🔥

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -17,8 +17,6 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-
-
 ┈┈┈┈╱▔▔▔▔╲┈┈┈┈
 ┈┈┈▕▕╲┊┊╱▏▏┈┈┈
 ┈┈┈▕▕▂╱╲▂▏▏┈┈┈
