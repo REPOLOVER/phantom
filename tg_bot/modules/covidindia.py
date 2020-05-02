4081dea8aa4f2c62a51e26c8fd1809fb6f2239c5
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-05-02 21:20:22 +0530

    🔥

--- tg_bot/modules/covidindia.py
+++ tg_bot/modules/covidindia.py
@@ -48,7 +48,7 @@ __help__ = """
  - /covindia <state>: Get real time COVID-19 stats for the input Indian state
 """
 
-__mod_name__ = 'COVID-19 Tracker'
+__mod_name__ = 'COVID-19 VIRUS'
 
 COV_INDIA_HANDLER = CommandHandler('covindia', covindia)
 
