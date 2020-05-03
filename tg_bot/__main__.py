44d0887e47da82c6dcb1e402848dda3a1ac49e2a
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-05-03 20:51:46 +0530

    🔥

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -151,7 +151,10 @@ def start(bot: Bot, update: Update, args: List[str]):
             first_name = update.effective_user.first_name
             update.effective_message.reply_text(
                 PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_ID),
-                parse_mode=ParseMode.MARKDOWN, disable_web_page_preview=True)
+                parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup([[InlineKeyboardButton(text="ADD ME TO YOUR GROUP",
+                                                                       url="t.me/{}?startgroup=true".format(bot.username))]]))
+
+
     else:
         update.effective_message.reply_text("Yo, whadup?🤧")
 
