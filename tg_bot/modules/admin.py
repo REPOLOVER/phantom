a75333695613b16e3494ddcf9ecf235c748ce89e
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-05-03 13:50:29 +0530

    🔥

--- tg_bot/modules/admin.py
+++ tg_bot/modules/admin.py
@@ -9,7 +9,7 @@ from telegram.ext import CommandHandler, Filters
 from telegram.ext.dispatcher import run_async
 from telegram.utils.helpers import escape_markdown, mention_html
 
-from tg_bot import dispatcher
+from tg_bot import dispatcher, TOKEN
 from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import bot_admin, can_promote, user_admin, can_pin
 from tg_bot.modules.helper_funcs.extraction import extract_user, extract_user_and_text
