cbf22e8babf0e0b719653479993a4527511627d7
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-05-03 20:49:28 +0530

    🔥

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -14,8 +14,11 @@ from tg_bot import dispatcher, updater, TOKEN, WEBHOOK, OWNER_ID, CERT_PATH, POR
 # needed to dynamically load modules
 # NOTE: Module order is not guaranteed, specify that in the config file!
 from tg_bot.modules import ALL_MODULES
+from tg_bot.modules.disable import DisableAbleCommandHandler
 from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
+
+
 
 PM_START_TEXT = """
 
