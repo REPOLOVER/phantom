497d68111112d1927d6f3d031bf2c0d8fa8fe55f
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-05-06 15:33:11 +0530

    🔥

--- tg_bot/modules/helper_funcs/alternate.py
+++ tg_bot/modules/helper_funcs/alternate.py
@@ -7,3 +7,27 @@ def send_message(message, text, *args, **kwargs):
     except error.BadRequest as err:
         if str(err) == "Reply message not found":
             return message.reply_text(text, quote=False, *args, **kwargs)
+
+
+def typing_action(func):
+    """Sends typing action while processing func command."""
+
+    @wraps(func)
+    def command_func(update, context, *args, **kwargs):
+        context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
+        return func(update, context,  *args, **kwargs)
+
+    return command_func
+
+
+def send_action(action):
+    """Sends `action` while processing func command."""
+
+    def decorator(func):
+        @wraps(func)
+        def command_func(update, context, *args, **kwargs):
+            context.bot.send_chat_action(chat_id=update.effective_chat.id, action=action)
+            return func(update, context,  *args, **kwargs)
+        return command_func
+
+    return decorator
