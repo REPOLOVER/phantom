3eb004057cdcbd8eb278ba6851060e92614d29b6
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-05-06 17:37:39 +0530

    🔥

--- tg_bot/modules/helper_funcs/alternate.py
+++ tg_bot/modules/helper_funcs/alternate.py
@@ -5,39 +5,14 @@ from functools import wraps
 from typing import Optional
 
 from telegram import User, Chat, ChatMember, Update, Bot
-from telegram import error, ChatAction
+from telegram import error
 
 from tg_bot import DEL_CMDS, SUDO_USERS, WHITELIST_USERS
 
 
-
-def send_message(message, text, *args, **kwargs):
-    try:
-        return message.reply_text(text, *args, **kwargs)
-    except error.BadRequest as err:
-        if str(err) == "Reply message not found":
-            return message.reply_text(text, quote=False, *args, **kwargs)
-
-
-def typing_action(func):
-    """Sends typing action while processing func command."""
-
-    @wraps(func)
-    def command_func(update, bot, *args, **kwargs):
-        bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
-        return func(update, bot,  *args, **kwargs)
-
-    return command_func
-
-
-def send_action(action):
-    """Sends `action` while processing func command."""
-
-    def decorator(func):
-        @wraps(func)
-        def command_func(update, bot, *args, **kwargs):
-            bot.send_chat_action(chat_id=update.effective_chat.id, action=action)
-            return func(update, bot,  *args, **kwargs)
-        return command_func
-
-    return decorator
+def send_message(message, text,  *args,**kwargs):
+	try:
+		return message.reply_text(text, *args,**kwargs)
+	except error.BadRequest as err:
+		if str(err) == "Reply message not found":
+			return message.reply_text(text, quote=False, *args,**kwargs)
