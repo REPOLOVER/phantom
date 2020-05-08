80f2d4969334b8af8c3508d0bd035ecc50d26fcb
leobrownlee <54571037+leobrownlee@users.noreply.github.com>
2020-05-08 12:01:48 +0530

    🔥

--- tg_bot/modules/sql/locks_sql.py
+++ tg_bot/modules/sql/locks_sql.py
@@ -67,6 +67,7 @@ class Restrictions(BASE):
 Permissions.__table__.create(checkfirst=True)
 Restrictions.__table__.create(checkfirst=True)
 
+
 PERM_LOCK = threading.RLock()
 RESTR_LOCK = threading.RLock()
 
